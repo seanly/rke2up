8e984b14e5d3cdb77280eedb82241ce41664a5c5
Seanly Lau <seanly@aliyun.com>
2023-05-11 01:09:38 -0400

    add  config

--- init.sh
+++ init.sh
@@ -10,7 +10,9 @@ export INSTALL_RKE2_AGENT_IMAGES_DIR=$(pwd)/v1.23/images
 mkdir -p /etc/rancher/rke2
 mkdir -p /var/lib/rancher/rke2/agent/images
 
-rsync -av ./etc/config.yaml /etc/rancher/rke2/
+INSTALL_RKE2_TYPE=$1
+
+rsync -av ./${INSTALL_RKE2_TYPE}/etc/config.yaml /etc/rancher/rke2/
 rsync -av ${INSTALL_RKE2_AGENT_IMAGES_DIR}/ /var/lib/rancher/rke2/agent/images
 
 ./scripts/install.sh
