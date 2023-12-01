4428dea54ea6f70423f13dac6f621a034ad853be
Ys Liu <seanly@opsbox.dev>
2023-12-01 03:43:54 +0000

    Add calico extra config

--- rke2up.sh
+++ rke2up.sh
@@ -15,8 +15,10 @@ export INSTALL_RKE2_AGENT_IMAGES_DIR=${CURRENT_DIR}/${INSTALL_RKE2_VERSION}/imag
 ## stage1: init dir
 _rke2_etc_path=/etc/rancher/rke2
 _rke2_images_path=/var/lib/rancher/rke2/agent/images
+_rke2_manifests_path=/var/lib/rancher/rke2/server/manifests/
 mkdir -p ${_rke2_etc_path}
 mkdir -p ${_rke2_images_path}
+mkdir -p ${_rke2_manifests_path}
 
 ## stage2: copy config
 rsync -av ./etc/${INSTALL_RKE2_TYPE}/config.yaml ${_rke2_etc_path}
@@ -24,7 +26,10 @@ rsync -av ./etc/${INSTALL_RKE2_TYPE}/config.yaml ${_rke2_etc_path}
 ## stage3: copy local images
 rsync -av ${INSTALL_RKE2_AGENT_IMAGES_DIR}/ ${_rke2_images_path}
 
-## stage4: install cluster
+## stage4: copy manifests
+rsync -av ./manifests/ ${_rke2_manifests_path}
+
+## stage5: install cluster
 bash ${CURRENT_DIR}/lib/install.sh
 
 modprobe ip_vs
@@ -32,9 +37,5 @@ modprobe ip_vs_rr
 modprobe ip_vs_wrr
 modprobe ip_vs_sh
 
-lsmod | grep ip_vs
-ls /lib/modules/$(uname -r)/kernel/net/netfilter/ipvs|grep -o "^[^.]*" > /etc/modules-load.d/ipvs.conf
-
-setenforce 0
 systemctl start rke2-${INSTALL_RKE2_TYPE}
 journalctl -f -u rke2-${INSTALL_RKE2_TYPE}
